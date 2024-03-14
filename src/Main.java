7ba4200c880dac30f299c5bde62ed7d1ec4a1763
MoFares7 <faresdabbas1@gmail.com>
2024-03-14 17:28:08 +0300

    solve squeer black

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        String t = scanner.next();
-        int sum = 0;
+        ArrayList<Integer> l = new ArrayList<>();
+        int r = 0;
 
-
-        for (int i = 0; i < n - 1; i++) {
-            if (t.charAt(i) == t.charAt(i+1)) {
-                sum++;
-            }
-
+        for (int i = 0; i < 4; i++) {
+            int n = scanner.nextInt();
+            l.add(n);
         }
-        System.out.println(sum);
+
+        String c = scanner.next();
+
+        for (int i = 0; i < c.length(); i++) {
+            int e = Integer.parseInt(String.valueOf(c.charAt(i)));
+
+            switch (e) {
+                case 1: {
+                    r += l.get(0);
+                    break;
+                }
+                case 2: {
+                    r += l.get(1);
+                    break;
+                }
+                case 3: {
+                    r += l.get(2);
+                    break;
+                }
+                case 4: {
+                    r += l.get(3);
+                    break;
+                }
+            }
+        }
+        System.out.println(r);
     }
 }
